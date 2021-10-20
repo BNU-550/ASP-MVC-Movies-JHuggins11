c04848abd8a1f5129ad160c4f6877f85adcce8c1
Jason Huggins <58664223+JHuggins11@users.noreply.github.com>
2021-10-20 17:44:04 +0100

    Followed tutorial video.

--- ASP-MVC-Movies/Data/ApplicationDbContext.cs
+++ ASP-MVC-Movies/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using ASP_MVC_Movies.Models;
 
 namespace ASP_MVC_Movies.Data
 {
@@ -12,5 +13,6 @@ namespace ASP_MVC_Movies.Data
             : base(options)
         {
         }
+        public DbSet<Movie> Movie { get; set; }
     }
 }
---
+++ ASP-MVC-Movies/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ASP_MVC_Movies.Data;
+using ASP_MVC_Movies.Models;
+
+namespace ASP_MVC_Movies.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ASP_MVC_Movies.Data.ApplicationDbContext _context;
+
+        public IndexModel(ASP_MVC_Movies.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
